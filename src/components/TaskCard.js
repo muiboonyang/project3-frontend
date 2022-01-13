9ac8f3f42343b6a363a8d384d44e2efa551b8176
Ernest <muiboonyang@gmail.com>
2022-01-13 13:19:45 +0800

    move server files to project3-backend

--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -1,14 +1,19 @@
 import React from "react";
-// import styles from "./TaskCard.module.css";
+import styles from "./TaskCard.module.css";
 
 const TaskCard = (props) => {
   return (
-    <div>
-      Name: {props.tasks.name}
-      <br />
-      Email: {props.tasks.email}
-      <br />
-      Comments: {props.tasks.comments}
+    <div className={styles.container}>
+      <p>Name: {props.tasks.name}</p>
+
+      <p>Email: {props.tasks.email}</p>
+
+      <p>
+        Type:{" "}
+        {props.tasks.type.charAt(0).toUpperCase() + props.tasks.type.slice(1)}
+      </p>
+
+      <p>Comments: {props.tasks.comments}</p>
     </div>
   );
 };
