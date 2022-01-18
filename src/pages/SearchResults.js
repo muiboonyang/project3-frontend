a8a5f6c4f016c508ff5fa631ca0e2e43c5bafbae
cgyx <momgogogo@gmail.com>
2022-01-18 11:40:16 +0800

    put back search results

--- src/pages/SearchResults.js
+++ src/pages/SearchResults.js
@@ -5,7 +5,7 @@ import { v4 as uuidv4 } from "uuid";
 import Row from "react-bootstrap/Row";
 import SearchCard from "../components/SearchCard.js";
 // import styles from "./SearchResults.module.css";
-
+//put back searchresults
 const SearchResults = () => {
   const [requests, setRequests] = useState([]);
   const params = useParams();
