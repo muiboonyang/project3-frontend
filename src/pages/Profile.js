158f13a94adbcb1456a78ccdf52d8bc4a2963851
cgyx <momgogogo@gmail.com>
2022-01-18 17:45:33 +0800

    my task and profile layout

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
 import LoginContext from "../context/login-context";
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
 // import styles from "./Profile.module.css";
 
 const Profile = () => {
@@ -29,16 +31,39 @@ const Profile = () => {
   }, []);
 
   return (
-    <>
-      Username: {userInfo.username} <br />
-      <br />
-      Name: {userInfo.name} <br />
-      Email: {userInfo.email} <br />
-      Contact: {userInfo.contact} <br />
-      Address: {userInfo.address} <br />
-      Unit: {userInfo.unit} <br />
-      Zip Code: {userInfo.zipcode} <br />
-    </>
+    <Tabs defaultActiveKey="1">
+      <Tab eventKey="1" title="Username">
+        Username: {userInfo.username}
+      </Tab>
+      <Tab eventKey="2" title="Name">
+        Name: {userInfo.name}
+      </Tab>
+      <Tab eventKey="3" title="Email">
+        Email: {userInfo.email}
+      </Tab>
+      <Tab eventKey="4" title="Contact">
+        Contact: {userInfo.contact}
+      </Tab>
+      <Tab eventKey="5" title="Address">
+        Address: {userInfo.address}
+      </Tab>
+      <Tab eventKey="6" title="Unit">
+        Unit: {userInfo.unit}
+      </Tab>
+      <Tab eventKey="7" title="Zip-code">
+        Zip-code: {userInfo.zipcode}
+      </Tab>
+    </Tabs>
+    // <>
+    //   Username: {userInfo.username} <br />
+    //   <br />
+    //   Name: {userInfo.name} <br />
+    //   Email: {userInfo.email} <br />
+    //   Contact: {userInfo.contact} <br />
+    //   Address: {userInfo.address} <br />
+    //   Unit: {userInfo.unit} <br />
+    //   Zip Code: {userInfo.zipcode} <br />
+    // </>
   );
 };
 
